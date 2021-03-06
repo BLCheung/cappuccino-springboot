c355bd456a7b06d61cdb2083c84e524eb533ef55
BLCheung <blcheung@qq.com>
2021-03-06 17:52:34 +0800

    采用yml配置文件；

---
+++ src/main/java/com/blcheung/missyou/model/BannerModel.java
@@ -0,0 +1,9 @@
+package com.blcheung.missyou.model;
+
+public class BannerModel {
+    private long id;
+    private String name;
+    private String description;
+    private String img;
+    private String title;
+}
--- src/main/java/com/blcheung/missyou/service/BannerServiceImpl.java
+++ src/main/java/com/blcheung/missyou/service/BannerServiceImpl.java
@@ -1,5 +1,8 @@
 package com.blcheung.missyou.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BannerServiceImpl implements BannerService {
     @Override
     public void getByName(String name) {
