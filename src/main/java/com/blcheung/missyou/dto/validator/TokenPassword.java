b1009ad308f7097e962748e214831bb2dc8a3d8f
BLCheung <blcheung@qq.com>
2021-08-12 23:01:37 +0800

    增加登录token密码校验器；

---
+++ src/main/java/com/blcheung/missyou/dto/validator/TokenPassword.java
@@ -0,0 +1,21 @@
+package com.blcheung.missyou.dto.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.TYPE })
+@Documented
+@Constraint(validatedBy = TokenPasswordValidator.class)
+public @interface TokenPassword {
+    String message() default "{token.password}";
+
+    int min() default 6;
+
+    int max() default 32;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/blcheung/missyou/dto/validator/TokenPasswordValidator.java
@@ -0,0 +1,25 @@
+package com.blcheung.missyou.dto.validator;
+
+import org.springframework.util.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class TokenPasswordValidator implements ConstraintValidator<TokenPassword, String> { // A: 关联的注解，T: 被校验的字段类型
+    private int min;
+    private int max;
+
+    @Override
+    public void initialize(TokenPassword constraintAnnotation) {
+        this.min = constraintAnnotation.min();
+        this.max = constraintAnnotation.max();
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        // 如果没有密码，让它通过，小程序没有密码
+        if (StringUtils.isEmpty(s)) return true;
+
+        return s.length() >= this.min && s.length() <= this.max;
+    }
+}
