7b915b8868e3bbdf4501f2f38960a7338ecd9082
BLCheung <blcheung@qq.com>
2021-09-03 00:23:48 +0800

    新增全局请求统一结果类与辅助类；

---
+++ src/main/java/com/blcheung/missyou/kit/ResultKit.java
@@ -0,0 +1,24 @@
+package com.blcheung.missyou.kit;
+
+import com.blcheung.missyou.common.Result;
+import com.blcheung.missyou.core.configuration.ExceptionCodeConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResultKit {
+    private static ExceptionCodeConfiguration codeConfiguration;
+
+    @Autowired
+    public void setCodeConfiguration(ExceptionCodeConfiguration codeConfiguration) {
+        ResultKit.codeConfiguration = codeConfiguration;
+    }
+
+    public static <T> Result<T> setResult(T data) {
+        return new Result<>(0, codeConfiguration.getMessage(0), data);
+    }
+
+    public static Result error() {
+        return new Result(9999, codeConfiguration.getMessage(9999), null);
+    }
+}
