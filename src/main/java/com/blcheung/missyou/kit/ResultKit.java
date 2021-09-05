5ea808fa6c6e7ed1ee77df7a34bbd7fc8dc8e573
BLCheung <blcheung@qq.com>
2021-09-05 23:56:06 +0800

    调整全局结果封装方法；

--- src/main/java/com/blcheung/missyou/kit/ResultKit.java
+++ src/main/java/com/blcheung/missyou/kit/ResultKit.java
@@ -5,6 +5,9 @@ import com.blcheung.missyou.core.configuration.ExceptionCodeConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * 提供给业务的结果封装类
+ */
 @Component
 public class ResultKit {
     private static ExceptionCodeConfiguration codeConfiguration;
@@ -18,7 +21,11 @@ public class ResultKit {
         return new Result<>(0, codeConfiguration.getMessage(0), data);
     }
 
-    public static Result error() {
+    public static Result reject() {
         return new Result(9999, codeConfiguration.getMessage(9999), null);
+    }
+
+    public static Result reject(Integer code, String msg) {
+        return new Result(code, msg, null);
     }
 }
