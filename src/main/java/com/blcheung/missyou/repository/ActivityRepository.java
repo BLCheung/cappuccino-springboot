5dfcd098731125bfb275bbf56d0c84a8eea974c3
BLCheung <blcheung@qq.com>
2021-09-25 00:17:07 +0800

    活动仓储新增查询优惠券；

--- src/main/java/com/blcheung/missyou/repository/ActivityRepository.java
+++ src/main/java/com/blcheung/missyou/repository/ActivityRepository.java
@@ -1,8 +1,11 @@
 package com.blcheung.missyou.repository;
 
 import com.blcheung.missyou.model.Activity;
+import com.blcheung.missyou.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     Activity findByName(String name);
+
+    Activity findByCouponListId(Long couponId);
 }
