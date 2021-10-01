7b597dc11ab0a3e6f27e011ba68595f2e718b797
BLCheung <blcheung@qq.com>
2021-10-02 03:28:13 +0800

    新增用户收货地址api；

---
+++ src/main/java/com/blcheung/missyou/api/v1/UserController.java
@@ -0,0 +1,26 @@
+package com.blcheung.missyou.api.v1;
+
+import com.blcheung.missyou.common.Result;
+import com.blcheung.missyou.core.annotations.ScopeLevel;
+import com.blcheung.missyou.kit.ResultKit;
+import com.blcheung.missyou.model.UserAddress;
+import com.blcheung.missyou.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/address")
+    @ScopeLevel()
+    private Result<List<UserAddress>> getUserAddress() {
+        return ResultKit.resolve(this.userService.getUserAddress());
+    }
+}
--- src/main/java/com/blcheung/missyou/service/UserService.java
+++ src/main/java/com/blcheung/missyou/service/UserService.java
@@ -1,14 +1,34 @@
 package com.blcheung.missyou.service;
 
+import com.blcheung.missyou.exception.http.ParameterException;
+import com.blcheung.missyou.kit.LocalUserKit;
 import com.blcheung.missyou.model.User;
+import com.blcheung.missyou.model.UserAddress;
+import com.blcheung.missyou.repository.UserAddressRepository;
 import com.blcheung.missyou.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository        userRepository;
+    @Autowired
+    private UserAddressRepository userAddressRepository;
 
     public User getUserById(Long uid) { return this.userRepository.findFirstById(uid); }
+
+    public List<UserAddress> getUserAddress() {
+        Long userId = LocalUserKit.getUser()
+                                  .getId();
+
+        List<UserAddress> userAddressList = this.userAddressRepository.findAllByUserIdOrderByUpdateTimeDesc(userId);
+        if (userAddressList.isEmpty()) return Collections.emptyList();
+
+        return userAddressList;
+    }
 }
