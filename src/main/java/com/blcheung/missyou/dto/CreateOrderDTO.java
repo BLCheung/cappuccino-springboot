b560e49a672b05951ffbe240409f41f94dcc260f
BLCheung <blcheung@qq.com>
2021-10-02 03:29:19 +0800

    新增订单DTO对象；

---
+++ src/main/java/com/blcheung/missyou/dto/CreateOrderDTO.java
@@ -0,0 +1,21 @@
+package com.blcheung.missyou.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * 下单时传递的订单DTO
+ */
+@Getter
+@Setter
+public class CreateOrderDTO {
+    @NotNull(message = "购买的商品集合不能为空")
+    private List<SkuDTO> skuList;
+    @NotNull(message = "收货地址不能为空")
+    private Long         addressId;
+    private Long         couponId;
+    private String       remark;
+}
