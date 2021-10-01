0004e0dcae615951a722f3f46f48feaceeed42ab
BLCheung <blcheung@qq.com>
2021-10-02 04:26:50 +0800

    新增获取用户收货地址；

---
+++ src/main/java/com/blcheung/missyou/api/v1/OrderController.java
@@ -0,0 +1,44 @@
+package com.blcheung.missyou.api.v1;
+
+import com.blcheung.missyou.common.Result;
+import com.blcheung.missyou.core.annotations.ScopeLevel;
+import com.blcheung.missyou.dto.CreateOrderDTO;
+import com.blcheung.missyou.kit.LocalUserKit;
+import com.blcheung.missyou.kit.ResultKit;
+import com.blcheung.missyou.logic.OrderChecker;
+import com.blcheung.missyou.model.UserAddress;
+import com.blcheung.missyou.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 获取用户下单时的收货地址
+     *
+     * @return
+     */
+    @GetMapping("/address")
+    @ScopeLevel()
+    public Result<UserAddress> getUserOrderAddress() {
+        List<UserAddress> addressList = this.userService.getUserAddress();
+
+        if (addressList.isEmpty()) return ResultKit.resolve(null);
+
+        // 过滤出默认地址，如果没有则选最新的收货地址
+        UserAddress userAddress = addressList.stream()
+                                             .filter(UserAddress::getIsDefault)
+                                             .findFirst()
+                                             .orElse(addressList.get(0));
+
+        return ResultKit.resolve(userAddress);
+    }
+
+}
