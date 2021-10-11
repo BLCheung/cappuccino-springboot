505fe8e2cbab5e7e8247c466008e8f69a668c88d
BLCheung <blcheung@qq.com>
2021-10-12 01:43:22 +0800

    新增价格运算工具；

---
+++ src/main/java/com/blcheung/missyou/kit/MoneyKit.java
@@ -0,0 +1,44 @@
+package com.blcheung.missyou.kit;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * 金额计算工具类
+ */
+// @ConfigurationProperties(prefix = "zbl.money")
+@Component
+public class MoneyKit {
+    // 价格运算取整模式
+    private static int mode;
+
+    @Value("${zbl.money.mode}")
+    public void setMode(int mode) {
+        MoneyKit.mode = mode;
+    }
+
+    /**
+     * 计算满减价
+     *
+     * @param originPrice 原价
+     * @param minusPrice  满减整数价
+     * @return
+     */
+    public static BigDecimal discountMinusPrice(BigDecimal originPrice, BigDecimal minusPrice) {
+        return originPrice.subtract(minusPrice);
+    }
+
+    /**
+     * 计算满减折扣价
+     *
+     * @param originPrice 原价
+     * @param offRate     折扣率（小数）
+     * @return
+     */
+    public static BigDecimal discountOffPrice(BigDecimal originPrice, BigDecimal offRate) {
+        return originPrice.multiply(offRate)
+                          .setScale(2, MoneyKit.mode);
+    }
+}
