b53823d5362f866ed02d07a2216eb11c99a25cbb
BLCheung <blcheung@qq.com>
2021-10-18 00:32:19 +0800

    no message

--- src/main/java/com/blcheung/missyou/repository/CouponRepository.java
+++ src/main/java/com/blcheung/missyou/repository/CouponRepository.java
@@ -20,7 +20,7 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     @Query("select c from Coupon c\n" + "join UserCoupon uc\n" + "on c.id = uc.couponId\n" + "join User u\n" +
            "on u.id = uc.userId\n" + "where u.id = :uid\n" + "and uc.status = 0\n" + "and uc.orderId is null\n" +
-           "and c.startTime < :now\n" + "and c.endTime > :now\n" + "\n")
+           "and c.startTime < :now\n" + "and c.endTime > :now\n")
     List<Coupon> findAllMyAvailableCoupon(Long uid, Date now);
 
     @Query("select c from Coupon c\n" + "join UserCoupon uc\n" + "on c.id = uc.couponId\n" + "join User u\n" +
