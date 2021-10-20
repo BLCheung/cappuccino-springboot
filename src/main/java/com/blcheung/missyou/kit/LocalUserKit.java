63da2a9a1aecb2b974bef10b9a3e77c8e2e3fb9f
BLCheung <blcheung@qq.com>
2021-10-20 18:01:38 +0800

    增加用户校验；

--- src/main/java/com/blcheung/missyou/kit/LocalUserKit.java
+++ src/main/java/com/blcheung/missyou/kit/LocalUserKit.java
@@ -1,13 +1,15 @@
 package com.blcheung.missyou.kit;
 
+import com.blcheung.missyou.exception.http.ForbiddenException;
+import com.blcheung.missyou.exception.http.NotFoundException;
 import com.blcheung.missyou.model.User;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class LocalUserKit {
     private static ThreadLocal<Map<String, Object>> threadLocal = new ThreadLocal<>();
-
 
     public static void init(User user, Integer scope) {
         Map<String, Object> map = new HashMap<>();
@@ -16,14 +18,26 @@ public class LocalUserKit {
         threadLocal.set(map);
     }
 
+    /**
+     * 获取当前请求的用户
+     *
+     * @return User
+     */
     public static User getUser() {
         Map<String, Object> map = threadLocal.get();
-        return (User) map.get("user");
+        Optional<User> userOptional = Optional.ofNullable((User) map.get("user"));
+        return userOptional.orElseThrow(() -> new NotFoundException(20002));
     }
 
+    /**
+     * 获取用户的权限级别
+     *
+     * @return User的scope权限
+     */
     public static Integer getScope() {
         Map<String, Object> map = threadLocal.get();
-        return (Integer) map.get("scope");
+        Optional<Integer> scopeOptional = Optional.ofNullable((Integer) map.get("scope"));
+        return scopeOptional.orElseThrow(() -> new ForbiddenException(10005));
     }
 
     public static void clear() { threadLocal.remove(); }
